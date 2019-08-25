81d4bd61246d460fd16bda13df129df1cecbd015
Eduardo Fernandes <eduardo.yugan@gmail.com>
2019-08-25 18:10:56 -0400

    alterado variaveis de cache do sw

--- src/app/view/service-worker.js
+++ src/app/view/service-worker.js
@@ -3,17 +3,14 @@ const staticCache = "Socket.io 25-08-2019"
 //lista de arquivos que devem ser cacheados
 const files = [
   './assets/css/bootstrap.min.css',
-  './assets/css/chat.css',
-  './assets/css/home.css',
   './assets/css/index.css',
-  './assets/css/register_login.css',
   './assets/js/axios.min.js',
   './assets/js/bootstrap.min.js',
   './assets/js/index.js',
   './assets/js/jquery.min.js',
   './assets/js/socketio.min.js',
   './assets/js/userSocket.js',
-  './assets/img/node.png',
+  './assets/img/node.webp',
 ]
 
 // Faz cache dos arquivos ao instalar 
